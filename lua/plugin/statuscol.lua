10db6ee2bc63bd2edfc3343b96100b75d010c550
9vvert <mzhm803@gmail.com>
2025-10-17 09:40:12 +0800

    add Trouble plugin

--- lua/plugin/statuscol.lua
+++ lua/plugin/statuscol.lua
@@ -3,30 +3,33 @@ return {
     config = function()
         -- Custom function to show both absolute and relative line numbers
         local function lnum_both()
-        local lnum = vim.v.lnum
-        local relnum = vim.v.lnum == vim.fn.line(".") and 0 or math.abs(vim.v.lnum - vim.fn.line("."))
-        return string.format("%3d %2d", lnum, relnum)
+            local lnum = vim.v.lnum
+            local relnum = vim.v.lnum == vim.fn.line(".") and 0 or math.abs(vim.v.lnum - vim.fn.line("."))
+            return string.format("%3d %2d", lnum, relnum)
         end
         require("statuscol").setup({
             setopt = true,
             segments = {
-                
-                {
-                    text = { lnum_both, " " },
-                    condition = { true },
-                    click = "v:lua.ScLa",
-                },
-                {
-                    sign = {
-                        namespace = { "gitsigns.*" },
-                        name = { "gitsigns.*" },
-                    },
-                },
+                -- error/warnning symbols
                 {
                     sign = {
                         namespace = { ".*" },
                         name = { ".*" },
                         auto = true,
+                    },
+                },
+                -- number lines
+
+                {
+                    text = { lnum_both, " " },
+                    condition = { true },
+                    click = "v:lua.ScLa",
+                },
+                -- git signs
+                {
+                    sign = {
+                        namespace = { "gitsigns.*" },
+                        name = { "gitsigns.*" },
                     },
                 },
             },

c23ba9399726c275feee184a2128cda1ec465a94
9vvert <mzhm803@gmail.com>
2025-10-28 14:50:03 +0800

    add something

--- lua/user/keymap.lua
+++ lua/user/keymap.lua
@@ -79,18 +79,23 @@ vim.keymap.set("n", "<leader>bod", "<cmd>BufferOrderByDirectory<CR>", { desc = "
 vim.keymap.set("n", "<leader>bol", "<cmd>BufferOrderByLanguage<CR>", { desc = "Buffer Order by Language" })
 vim.keymap.set("n", "<leader>bow", "<cmd>BufferOrderByWindowNumber<CR>", { desc = "Buffer Order by Window Number" })
 vim.keymap.set("n", "<leader>bob", "<cmd>BufferOrderByBufferNumber<CR>", { desc = "Buffer Order by Buffer Number" })
+
 -- move between buffers & adjust buffer position
 vim.keymap.set("n", "<M-,>", "<cmd>BufferPrevious<CR>", { desc = "Move to previous buffer" })
 vim.keymap.set("n", "<M-.>", "<cmd>BufferNext<CR>", { desc = "Move to next buffer" })
 vim.keymap.set("n", "<M-<>", "<cmd>BufferMovePrevious<CR>", { desc = "Swap with previous buffer" })
 vim.keymap.set("n", "<M->>", "<cmd>BufferMoveNext<CR>", { desc = "Swap with next buffer" })
+
 -- close/restore buffer
 vim.keymap.set("n", "<M-/>", "<cmd>BufferClose<CR>", { desc = "Close buffer" })
 vim.keymap.set("n", "<M-s-c>", "<cmd>BufferRestore<CR>", { desc = "Restore buffer" })
+
 -- magic buffer mode
 vim.keymap.set("n", "<leader>bs", "<cmd>BufferPick<CR>", { desc = "Select buffer in magic mode" })
 vim.keymap.set("n", "<leader>bk", "<cmd>BufferPickDelete<CR>", { desc = "Kill buffer in magic mode" })
+
 -- goto buffer in position
+
 vim.keymap.set("n", "<M-0>", "<cmd>BufferLast<CR>", { desc = "Goto last buffer" })
 vim.keymap.set("n", "<M-1>", "<cmd>BufferGoto 1<CR>", { desc = "goto buffer 1" })
 vim.keymap.set("n", "<M-2>", "<cmd>BufferGoto 2<CR>", { desc = "goto buffer 2" })
